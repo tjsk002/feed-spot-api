08d14c6c1ac9c2f983087f24b944aecfd02ed706
tjsk002 <tjsk002@naver.com>
2025-04-01 11:10:14 +0900

    feat: user request, response dto

---
+++ src/main/kotlin/com/sideproject/userInfo/userInfo/data/dto/UserRequestDto.kt
@@ -0,0 +1,34 @@
+package com.sideproject.userInfo.userInfo.data.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+
+
+
+class UserRequestDto(
+    val userData: UserRequest
+
+)
+
+class UserRequest(
+    @field:NotBlank(message = "username is required value.")
+    @field:Size(min = 5, max = 12, message = "username is between 5 and 12.")
+    val username: String,
+
+    @field:NotBlank(message = "nickName is required value.")
+    val nickName: String,
+
+    @field:Pattern(regexp = "^(female|male)$", message = "gender must be either 'female' or 'male'.")
+    val gender: String,
+
+    val isActive: Boolean,
+
+    @field:NotBlank(message = "type is required value")
+    @field:Pattern(regexp = "^(front|back|dba|infra)$", message = "type must be one of the following values: 'front', 'back', 'dba', or 'infra'."
+    )
+    val type: String,
+
+    val description: String?
+)
+
