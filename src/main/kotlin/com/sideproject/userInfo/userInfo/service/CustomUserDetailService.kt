2bc52e4ec2beaead003c0eedd0536c1ac4151b76
tjsk002 <tjsk002@naver.com>
2025-04-01 10:54:26 +0900

    feat: customDetailService (user, admin) 추가

--- src/main/kotlin/com/sideproject/userInfo/userInfo/service/CustomUserDetailService.kt
+++ src/main/kotlin/com/sideproject/userInfo/userInfo/service/CustomUserDetailService.kt
@@ -1,23 +1,23 @@
 package com.sideproject.userInfo.userInfo.service
 
-import com.sideproject.userInfo.userInfo.data.dto.admins.CustomAdminDetails
-import com.sideproject.userInfo.userInfo.data.entity.AdminsEntity
-import com.sideproject.userInfo.userInfo.repository.AdminsRepository
+import com.sideproject.userInfo.userInfo.data.entity.UsersEntity
+import com.sideproject.userInfo.userInfo.repository.UsersRepository
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 
 @Service
 @Transactional(readOnly = true)
 class CustomUserDetailsService(
-    private val adminsRepository: AdminsRepository
+    private val userRepository: UsersRepository,
 ) : UserDetailsService {
-    override fun loadUserByUsername(username: String): UserDetails {
-        val admin: AdminsEntity = adminsRepository.findByUsername(username)
-            ?: throw UsernameNotFoundException("존재하지 않는 관리자 회원")
-        return CustomAdminDetails(admin)
+
+    override fun loadUserByUsername(id: String): UserDetails? {
+        val user: UsersEntity = userRepository.findById(id.toLong())
+            .orElseThrow { UsernameNotFoundException("존재하지 않는 회원") }
+        return null;
     }
 }
