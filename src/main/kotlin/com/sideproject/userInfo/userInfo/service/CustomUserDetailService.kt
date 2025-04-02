2d045f44683c4dfb46be0bf91b0c12819dba6fba
tjsk002 <tjsk002@naver.com>
2025-04-02 16:43:05 +0900

    feat: CustomUserDetailsService 수정

--- src/main/kotlin/com/sideproject/userInfo/userInfo/service/CustomUserDetailService.kt
+++ src/main/kotlin/com/sideproject/userInfo/userInfo/service/CustomUserDetailService.kt
@@ -2,20 +2,18 @@ package com.sideproject.userInfo.userInfo.service
 
 import com.sideproject.userInfo.userInfo.data.dto.admins.CustomAdminDetails
 import com.sideproject.userInfo.userInfo.data.entity.AdminsEntity
-import com.sideproject.userInfo.userInfo.data.entity.UsersEntity
 import com.sideproject.userInfo.userInfo.repository.AdminsRepository
-import com.sideproject.userInfo.userInfo.repository.UsersRepository
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 
 @Service
 @Transactional(readOnly = true)
 class CustomUserDetailsService(
-    private val adminsRepository: AdminsRepository,
+    private val adminsRepository: AdminsRepository
 ) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
         val admin: AdminsEntity = adminsRepository.findByUsername(username)
