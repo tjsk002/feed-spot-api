050d2e354b47487b11bbc5321d180abae98af4d4
tjsk002 <tjsk002@naver.com>
2025-04-04 18:16:23 +0900

    feat: users 임의 데이터 role 데이터 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -13,8 +13,8 @@ VALUES ('user_acro_1', 'Nick1', 'male', TRUE, 'back', 'user_acro_ 1 description'
        ('user_acro_10', 'Nick10', 'female', FALSE, 'back', 'user_acro_ 10 description', NOW(), NOW(), null);
 
 INSERT INTO ADMINS (user_name, nick_name, role, password, created_at, updated_at, deleted_at)
-VALUES ('admin1', 'AdminNick1', 'ROLE_ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null),
-       ('admin2', 'AdminNick2', 'ROLE_ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null),
-       ('admin3', 'AdminNick3', 'ROLE_ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null),
-       ('admin4', 'AdminNick4', 'ROLE_ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null);
+VALUES ('admin1', 'AdminNick1', 'ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null),
+       ('admin2', 'AdminNick2', 'ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null),
+       ('admin3', 'AdminNick3', 'ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null),
+       ('admin4', 'AdminNick4', 'ADMIN', crypt('password1!', gen_salt('bf')), NOW(), NOW(), null);
 
