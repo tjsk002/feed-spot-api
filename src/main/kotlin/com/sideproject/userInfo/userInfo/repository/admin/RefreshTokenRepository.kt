433c2f86be903ac8ccb010c54549a343f8cbec5f
tjsk002 <tjsk002@naver.com>
2025-04-15 14:56:20 +0900

    feat: RefreshTokenRepository findByUser 추가

--- src/main/kotlin/com/sideproject/userInfo/userInfo/repository/admin/RefreshTokenRepository.kt
+++ src/main/kotlin/com/sideproject/userInfo/userInfo/repository/admin/RefreshTokenRepository.kt
@@ -2,8 +2,10 @@ package com.sideproject.userInfo.userInfo.repository.admin
 
 import com.sideproject.userInfo.userInfo.data.entity.AdminsEntity
 import com.sideproject.userInfo.userInfo.data.entity.RefreshTokenEntity
+import com.sideproject.userInfo.userInfo.data.entity.UsersEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RefreshTokenRepository : JpaRepository<RefreshTokenEntity, Long> {
     fun findByAdmin(admin: AdminsEntity): RefreshTokenEntity?
+    fun findByUser(user: UsersEntity): RefreshTokenEntity?
 }
