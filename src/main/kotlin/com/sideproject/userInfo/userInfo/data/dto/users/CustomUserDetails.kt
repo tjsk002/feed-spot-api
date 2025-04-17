8891ab0f63d834ab3df6f0878e6a9098fc4c6b59
tjsk002 <tjsk002@naver.com>
2025-04-17 17:57:35 +0900

    feat: CustomUserDetails 추가

---
+++ src/main/kotlin/com/sideproject/userInfo/userInfo/data/dto/users/CustomUserDetails.kt
@@ -0,0 +1,27 @@
+import com.sideproject.userInfo.userInfo.data.entity.UserEntity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class CustomUserDetails(private val user: UserEntity) : UserDetails {
+
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return listOf(SimpleGrantedAuthority("USER"))
+    }
+
+    override fun getPassword(): String {
+        return user.password
+    }
+
+    override fun getUsername(): String {
+        return user.username
+    }
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
