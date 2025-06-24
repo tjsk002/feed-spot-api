bc432b9f91efe76a2b247deeff5e21ca648e1207
tjsk002 <tjsk002@naver.com>
2025-06-24 09:36:03 +0900

    feat: 공지사항 조회수 증가 API

--- src/main/kotlin/com/sideproject/userInfo/userInfo/controller/NoticeController.kt
+++ src/main/kotlin/com/sideproject/userInfo/userInfo/controller/NoticeController.kt
@@ -1,14 +1,13 @@
 package com.sideproject.userInfo.userInfo.controller
 
 import com.sideproject.userInfo.userInfo.common.response.RestResponse
+import com.sideproject.userInfo.userInfo.data.dto.notice.ViewCountRequest
 import com.sideproject.userInfo.userInfo.service.NoticeService
+import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/notices")
@@ -27,5 +26,12 @@ class NoticeController(
         @PathVariable(required = true) noticeId: Long,
     ): ResponseEntity<RestResponse<Map<String, Any>>> {
         return ResponseEntity.ok(noticeService.getNoticeDetail(noticeId))
+    }
+
+    @PostMapping("/view-count")
+    fun increaseViewCount(
+        @RequestBody @Valid noticeDto: ViewCountRequest,
+    ): ResponseEntity<RestResponse<Map<String, Any>>> {
+        return ResponseEntity.ok(noticeService.increaseViewCount(noticeDto))
     }
 }
